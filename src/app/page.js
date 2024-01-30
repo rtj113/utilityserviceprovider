467f57517352329f920b4aadae3217f8626460cc
Rafael Torres <rtj113@gmail.com>
2024-01-30 14:07:44 -0800

    Reverting storyBlok changes that were migrated to another branch. Cleaning this up for exporting to a dist folder to put in AWS.

--- src/app/page.js
+++ src/app/page.js
@@ -9,10 +9,10 @@ export default function Home() {
       <UtilityCards />
       <section className="mb-2 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left text-black p-24">
           <h2 className="mb-2 text-2xl font-bold tracking-tight text-primary">
-          Explore CPA's service area
+          Explore CPA&apos;s service area
           </h2>
           <p>
-          Learn how CPA’s 32 cities and the unincorporated areas of Los Angeles and Ventura Counties lead the way to a clean energy future. Click on an icon below to discover how much Greenhouse Gas Emissions have been avoided in the communities we serve and more.
+          Learn how CPA&apos;s 32 cities and the unincorporated areas of Los Angeles and Ventura Counties lead the way to a clean energy future. Click on an icon below to discover how much Greenhouse Gas Emissions have been avoided in the communities we serve and more.
           </p>    
       </section>
     </main>
--- src/app/pages/api/submit.js
+++ src/app/pages/api/submit.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+export async function handler(req, res) {
     const data = req.body;
     const id = await createItem(data);
     res.status(200).json({id});
